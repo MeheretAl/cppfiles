e58862ecb8da960ba0cf206fca239d1dca0444db
MeheretAl <meheretx04@gmail.com>
2023-06-04 18:34:14 +0300

    first commit

--- hello.cpp
+++ hello.cpp
@@ -2,7 +2,22 @@
 
 using namespace std;
 
+bool check(string str){
+    for (int i = 0;i<str.length();i++)
+    if (isdigit(str[i])== false)
+    return false;
+    return true;
+    }
+
 int main() {
-    cout<<"Hello world";
-    return 0;
+    string str;
+    cout << "Input:\n";
+    cin >> str;
+    if (check(str)) {
+        cout<< str << "is an integer" <<endl;
+    }
+    else {
+        cout << str<< "is a string" <<endl;
+    }
+   return 0;
 }
