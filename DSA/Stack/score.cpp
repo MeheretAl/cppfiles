fc856674b9b31ef52d14005ff7974cb2127c9ce6
MeheretAl <meheretxo4@gmail.com>
2023-12-08 23:56:45 +0300

    added baseball leetcode problem

--- DSA/Stack/score.cpp
+++ DSA/Stack/score.cpp
@@ -4,53 +4,60 @@
 #include <iostream>
 
 using namespace std;
-class Solution {
+class Solution
+{
 public:
-    int calPoints(vector<string>& operations) {
-       stack<char> stack;
-    int total = 0;
-    string str = "";
-    for(string& c:operations){
-        str += c;
-    }
-    
-    for (int i = 0; i < str.size(); i++)
+    int calPoints(vector<string> &operations)
     {
-        if (isdigit(str[i]))
+        stack<char> stack;
+        int total = 0;
+        string str = "";
+        for (string &c : operations)
         {
-            int digit = str[i] - '0';
-            stack.push(digit);  
-            total += digit; 
-            cout << total << "checking/n";
-                   
-        } else if (str[i] == 'D')
-        {
-            stack.push(stack.top()*2);
-            total += stack.top()*2;
-            cout << total << "Checking/n";
-        } else if(str[i] == '+') {
-            int holder = stack.top();
-            stack.pop();
-            int holder2 = stack.top();
-            stack.push(holder);
-            stack.push(holder2);
-            cout << "checking at " << " holder 2" << total << endl;
-            total += holder + holder2;
-            stack.push(holder+holder2);
-        } else if(str[i] == 'C') {
-            total -= stack.top();
-            stack.pop();
-            cout << "checking at end" << total << endl;
-
+            str += c;
         }
-        
-        
-    }
-    return total;
+
+        for (int i = 0; i < str.size(); i++)
+        {
+            if (isdigit(str[i]))
+            {
+                int digit = str[i] - '0';
+                stack.push(digit);
+                cout << endl;
+                total += digit;
+                cout << total << "checking\n";
+            }
+            else if (str[i] == 'D')
+            {
+                stack.push(stack.top() * 2);
+                total += stack.top() * 2;
+                cout << total << " Checking\n";
+            }
+            else if (str[i] == '+')
+            {
+                int holder = stack.top();
+                stack.pop();
+                int holder2 = stack.top();
+                stack.push(holder);
+                stack.push(holder2);
+                cout << " checking at "
+                     << " holder 2 " << total << endl;
+                total += holder + holder2;
+                stack.push(holder + holder2);
+            }
+            else if (str[i] == 'C')
+            {
+                total -= stack.top();
+                stack.pop();
+                cout << " checking at end " << total << endl;
+            }
+        }
+        return total;
     }
 };
 
-int main() {
+int main()
+{
     Solution sol1;
     vector<string> operations;
     operations.push_back("5");
@@ -59,7 +66,4 @@ int main() {
     operations.push_back("D");
     operations.push_back("+");
     cout << sol1.calPoints(operations);
-
 }
-
-
---
+++ test2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int display(int array[],int size){
+
+}
+
+void extract(int num){
+     if (num<10)
+    {
+        cout << num << " ";
+    } else {
+        cout << num%10 << " ";
+        num/=10;
+        extract(num);
+    } 
+
+}
+
+int accept();
+
+int main(){
+    int* array = new int[]
+
+
+
+    return 0;
+}
