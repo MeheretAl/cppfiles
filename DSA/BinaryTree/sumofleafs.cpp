13531afa807fb392c3d115f47d3c050e5c75f5aa
MeheretAl <meheretxo4@gmail.com>
2024-03-21 11:44:43 +0300

    renamed trees to BinaryTree

--- DSA/BinaryTree/sumofleafs.cpp
+++ DSA/BinaryTree/sumofleafs.cpp
@@ -3,46 +3,45 @@
 
 using namespace std;
 
-int sumOfLeaf(Node* root){
+int sumOfLeaf(Node *root)
+{
     if (root == nullptr)
     {
         return 0;
     }
-    
+
     if (root != nullptr)
     {
         if (root->left == nullptr && root->right == nullptr)
         {
             return root->data;
         }
-        
     }
     int left = sumOfLeaf(root->left);
     int right = sumOfLeaf(root->right);
-    
-    return left+right;
+
+    return left + right;
 }
 
 int main(int argc, char const *argv[])
 {
-    Node* root = createNewNode(1);
+    Node *root = createNewNode(1);
     root->left = createNewNode(5);
     root->right = createNewNode(10);
-    root->left->left = createNewNode(8);//node
-    root->right->right = createNewNode(9);//node
-    root->right->right->left = createNewNode(14); //leaf
-    root->left->left->right = createNewNode(17); //leaf
-    root->right->left = createNewNode(78);//leaf
-    /* 
+    root->left->left = createNewNode(8);          // node
+    root->right->right = createNewNode(9);        // node
+    root->right->right->left = createNewNode(14); // leaf
+    root->left->left->right = createNewNode(17);  // leaf
+    root->right->left = createNewNode(78);        // leaf
+    /*
                             1
-                        
+
                     5               10
 
             8                78leaf           9
-                17leaf              14leaf     
+                17leaf              14leaf
     */
-    //sum of leaf should be 14+17+78 -> 109
-
+    // sum of leaf should be 14+17+78 -> 109
 
     int answer = sumOfLeaf(root);
     cout << answer;
