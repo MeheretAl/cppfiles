d9d184ee762dd24360cd03059cca3aa423cac646
MeheretAl <meheretxo4@gmail.com>
2024-04-10 02:16:10 +0300

    added removeDuplicates.cpp

---
+++ DSA/LinkedList/removeDuplicates.cpp
@@ -0,0 +1,32 @@
+#include "implementation.h"
+
+LinkedList *removeDuplicates(LinkedList *head)
+{
+    LinkedList *current = head;
+    if (head == nullptr || head->next == nullptr)
+    {
+        return head;
+    }
+
+    while (current != nullptr && current->next != nullptr)
+    {
+
+        if (current->data == current->next->data)
+        {
+            LinkedList *duplicate = current->next; // marking the node we want to delete as duplicate
+            current->next = duplicate->next;       // setting the next current ptr to the next duplicate ptr
+            // given  1->1->2->1
+            // duplicate points to the 1st index
+            // current->next was supposed to point to 1st index but we go one above to the 2nd index
+            // so our pointing structure? looks like 1->2->1 we've let go of 1
+
+            delete duplicate; // deleting 1 we longer need it or reference it again anywhere
+        }
+        else
+        {
+            current = current->next;
+        }
+    }
+
+    return head;
+}
