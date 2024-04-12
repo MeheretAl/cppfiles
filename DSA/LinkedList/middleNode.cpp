5676530f73254e9bf130ebf2fddc293092fae2a8
MeheretAl <meheretxo4@gmail.com>
2024-04-12 16:01:25 +0300

    segementation fault fixed

--- DSA/LinkedList/middleNode.cpp
+++ DSA/LinkedList/middleNode.cpp
@@ -1,18 +1,37 @@
 #include "implementation.h"
+#include <iostream>
 
 using namespace std;
 
 LinkedList *middleNode(LinkedList *head)
 {
-    LinkedList *fastptr, *slowptr = head;
+    LinkedList *fastptr = head;
+    LinkedList *slowptr = head;
     // fastptr != nullptr indicates fastptr has reached the end of the even sized linked list
     // and the slow ptr has reached the middle
     // fastptr.next != nullptr is for when the length of the linked list is odd
-    while (fastptr->next != nullptr && fastptr != nullptr)
+    while (fastptr != nullptr && fastptr->next != nullptr)
     {
         fastptr = fastptr->next->next;
         slowptr = slowptr->next;
     }
 
     return slowptr;
+}
+
+int main(int argc, char const *argv[])
+{
+    LinkedList *head = createNewLinkedList(0);
+    head->next = createNewLinkedList(1);
+    head->next->next = createNewLinkedList(2);
+    head->next->next->next = createNewLinkedList(4);
+
+    LinkedList *check = middleNode(head);
+    while (check != nullptr)
+    {
+        cout << check->data << endl;
+        check = check->next;
+    }
+
+    return 0;
 }
