77b93655ed961010b29bead9f0dc58594b3f2a38
MeheretAl <meheretxo4@gmail.com>
2024-04-17 11:54:34 +0300

    added mergeInBetweeenNodes.cpp

---
+++ DSA/LinkedList/mergeInBetweenNodes.cpp
@@ -0,0 +1,36 @@
+#include "implementation.h"
+
+ListNode *mergeInBetweenNodes(ListNode *list1, ListNode *list2, int a, int b)
+{
+    ListNode *NodeA = nullptr, *nodeB = nullptr, *ptr = list1;
+    int count = 0;
+    while (ptr != nullptr)
+    {
+
+        if (count == a - 1)
+        {
+            // getting nodeA to start connecting with list2
+            nodeA = ptr;
+        }
+        else if (count == b)
+        {
+            // saving nodeB for later use when connecting again
+            nodeB = ptr;
+            break;
+        }
+        count += 1;
+        ptr = ptr->next;
+    }
+    // connecting nodeA with list2
+    NodeA->next = list2;
+    ListNode *ptr2 = list2;
+    // finding the last node of list2 to connect back with list1
+    while (ptr2->next != nullptr)
+    {
+        ptr2 = ptr2->next;
+    }
+
+    ptr2->next = nodeB->next;
+
+    return list1;
+}
