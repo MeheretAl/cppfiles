cc4f0b7723897633d5ef6aa95f39bf2ce7ed75e1
MeheretAl <meheretxo4@gmail.com>
2024-04-21 10:06:43 +0300

    added more features to doublyLinkedList.h

--- DSA/LinkedList/doublyLinkedList.h
+++ DSA/LinkedList/doublyLinkedList.h
@@ -27,3 +27,65 @@ doublyLinkedList *insertNode(doublyLinkedList *node, int data)
 
     return newNode;
 }
+
+doublyLinkedList *insertNodeBefore(doublyLinkedList *node, int data)
+{
+    doublyLinkedList *newNode = new doublyLinkedList();
+    newNode->data = data;
+    newNode->prev = node->prev;
+    newNode->next = node->next;
+
+    if (node->prev == nullptr)
+    {
+        node->prev->next = newNode;
+    }
+    node->prev = newNode;
+
+    return newNode;
+}
+
+bool nodeExists(doublyLinkedList *node, int target)
+{
+    doublyLinkedList *ptr = node;
+    while (ptr != nullptr)
+    {
+        if (ptr->data == target)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+void deleteNodeByData(doublyLinkedList *node, int target)
+{
+    doublyLinkedList *ptr = node;
+    while (ptr != nullptr)
+    {
+        if (ptr->data == target)
+        {
+            // if the node before the current node isn't the head node
+            if (ptr->prev != nullptr)
+            {
+                ptr->prev->next = ptr->next;
+            }
+            // if the node after the current node isn't the tail node
+            if (ptr->next != nullptr)
+            {
+                ptr->next->prev = ptr->prev;
+            }
+
+            if (ptr == node)
+            {
+                node = ptr->next;
+            }
+
+            delete ptr;
+            return;
+        }
+        ptr = ptr->next;
+    }
+
+    cout << "Node with data " << target << " not found";
+}
