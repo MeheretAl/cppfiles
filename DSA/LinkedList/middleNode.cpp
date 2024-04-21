bcce3361347057a33aa88c17bfe54a7454d97175
MeheretAl <meheretxo4@gmail.com>
2024-04-21 10:18:50 +0300

    corrected comment

--- DSA/LinkedList/middleNode.cpp
+++ DSA/LinkedList/middleNode.cpp
@@ -7,9 +7,9 @@ ListNode *middleNode(ListNode *head)
 {
     ListNode *fastptr = head;
     ListNode *slowptr = head;
-    // fastptr != nullptr indicates fastptr has reached the end of the even sized linked list
+    // fastptr != nullptr indicates fastptr has reached the end of the linked list
     // and the slow ptr has reached the middle
-    // fastptr.next != nullptr is for when the length of the linked list is odd
+    // fastptr.next != nullptr is for when the length of the linked list is even
     while (fastptr != nullptr && fastptr->next != nullptr)
     {
         fastptr = fastptr->next->next;
